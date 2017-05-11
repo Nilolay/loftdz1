56fc2b57e7c3cee442c22e704e3de95ae35d0cfb
Nilolay <infinitygum@mail.ru>
2017-05-12 02:18:02 +0300

    6

--- src/index.js
+++ src/index.js
@@ -63,9 +63,10 @@ function returnCounter(number=0) {
  Функция должна привязать переданные аргументы к функции F и вернуть получившуюся функцию
  */
 function bindFunction(fn) {
-    return function () { 
-return fn.bind(null, arguments[0],arguments[1])();
-}; 
+   var res = function () { 
+    return fn.bind(null, arguments[0]);
+    }; 
+    return res();
 }
 
 export {
