33959bc1de6e379145405a1a5197975fb753c973
Nilolay <infinitygum@mail.ru>
2017-06-26 01:40:04 +0300

    1

--- src/index.js
+++ src/index.js
@@ -15,6 +15,24 @@ new Promise(function(resolve){
             	} else {
             		reject(new Error('Ты не пройдешь!'));
             	}
-            }, 2);
+            }, 2 | 4 );
     	});
+    })
+    .then(function() {
+        return new Promise(function(resolve, reject) {
+            VK.api( 'friends.get', {v: '5,64', fields: 'photo_100' }, function(response) {
+                if (response.error) {
+                    reject(new Error(response.error.error_msg));
+                } else {
+                    resolve(response);
+                }
+            });
+        });
+    })
+    .then(function(response) {
+        response.items.forEach(friend => {
+            var friendli = document.createElement('li');
+            friendli.classList.add('friend');
+//            список друзей .appendChild(friendli);
+        })
     })
