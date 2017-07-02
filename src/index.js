7280bf606b8503a3a439535a8db921b91b37717f
Nilolay <infinitygum@mail.ru>
2017-07-02 11:41:22 +0300

    Список друзей вк

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 var friendslist = document.getElementsByClassName('vk-friends')[0];
 var vkinput = document.getElementById('vk-filter');
 var myinput = document.getElementById('my-filter');
-
 new Promise(function(resolve){
 	window.addEventListener('load', function(){
 		resolve();
@@ -17,6 +16,7 @@ new Promise(function(resolve){
             	if (response.session) {
             		resolve(response);
             	} else {
+                    console.log(response);
             		reject(new Error('Ты не пройдешь!'));
             	}
             }, 2);
@@ -24,7 +24,7 @@ new Promise(function(resolve){
     })
     .then(function() {
         return new Promise(function(resolve, reject) {
-            VK.api( 'friends.get', {v: '5,64', fields: 'photo_100' }, function(response) {
+            VK.api( 'friends.get', {v: '5,65', fields: 'photo_50' }, function(response) {
                 if (response.error) {
                     reject(new Error(response.error.error_msg));
                 } else {
@@ -34,13 +34,12 @@ new Promise(function(resolve){
         });
     })
     .then(function(response) {
-        response.items.forEach(friend => {
+        response.response.items.forEach(friend => {
             var friendli = document.createElement('li');
-            var photo = new Image();
+            var photo = document.createElement('img');
             var nameSpan = document.createElement('span');
-            nameSpan.textContent = '${friend.first_name} ${friend.last_name}';
-            photo.src = friend.photo_100;
-            photo.width = '50px'
+            nameSpan.textContent = friend.first_name + ' ' + friend.last_name;
+            photo.src = friend.photo_50;
             friendli.classList.add('friend');
             friendli.appendChild(photo);
             friendli.appendChild(nameSpan);
